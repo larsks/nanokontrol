5412d323f40557e5a633119eb2e7ce5faf0651eb
Lars Kellogg-Stedman <lars@oddbit.com>
2020-04-14 19:49:44 -0400

    make sysex messages inherit from mido.Message

--- nanokontrol/sysex.py
+++ nanokontrol/sysex.py
@@ -1,19 +1,22 @@
 import mido
 
+__all__ = [
+    'SysexMessage',
+    'CurrentSceneRequest',
+]
 
-class Sysex(object):
-    @property
-    def message(self):
-        return mido.Message('sysex', data=self.bytes)
+
+class SysexMessage(mido.Message):
+    def __init__(self, data=None):
+        if data is None:
+            data = []
+
+        super().__init__('sysex', data=data)
 
 
-class CurrentSceneRequest(Sysex):
+class CurrentSceneRequest(SysexMessage):
     def __init__(self, channel=0):
-        self.channel = 0
-
-    @property
-    def bytes(self):
-        return [
-            0x42, 0x40 | self.channel, 0x00, 0x01, 0x13, 0x00,
+        super().__init__(data=[
+            0x42, 0x40 | channel, 0x00, 0x01, 0x13, 0x00,
             0x1f, 0x10, 0x00
-        ]
+        ])
